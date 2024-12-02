25f39b8db12b3cdfe6e0275271334b86de3fa183
Jamie McStay <jamiemcstay@hotmail.com>
2024-12-02 19:31:38 +0000

    Add SECRET_KEY to env.py and settings.py

--- tunecritic/settings.py
+++ tunecritic/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d&2$b!l0zt%=c#ny*pd@&4nwo-h@+!8ec4u6eme59#)kk)uc=5'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True 
 
 ALLOWED_HOSTS = ['8000-jamiemcstay-portfolio4-tlooqyh8svk.ws-eu117.gitpod.io',
 '.herokuapp.com']
