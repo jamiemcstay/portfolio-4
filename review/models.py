f9f47bcffa87d282547ae7b8a321c8725cced9e4
Jamie McStay <jamiemcstay@hotmail.com>
2024-12-03 19:35:00 +0000

    Add Review model

--- review/models.py
+++ review/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db.models import Q, CheckConstraint
 
 # Create your models here.
+
+
+class Review(models.Model):
+    id = models.AutoField(primary_key=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reviews")
+    album_name = models.CharField(max_length=255)
+    genre = models.CharField(max_length=50)
+    date_created = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    score = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+
+    class Meta:
+        constraints = [
+            CheckConstraint(
+                check=Q(score__gte=1) & Q(score__lte=5),
+                name="score_between_1_and_5"
+            )
+        ]
+
--- tunecritic/settings.py
+++ tunecritic/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True 
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-jamiemcstay-portfolio4-tlooqyh8svk.ws-eu117.gitpod.io',
 '.herokuapp.com']
@@ -89,6 +89,11 @@ WSGI_APPLICATION = 'tunecritic.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://8000-jamiemcstay-portfolio4-tlooqyh8svk.ws-eu117.gitpod.io",
+    "https://*.herokuapp.com"
+]
 
 
 # Password validation
