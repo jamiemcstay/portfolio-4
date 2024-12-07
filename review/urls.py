ab6f8ca3c71d583b2434a555813771bd4d6493db
Jamie McStay <jamiemcstay@hotmail.com>
2024-12-07 11:33:18 +0000

    Add review_detail function based view and review_detail.html template

--- review/urls.py
+++ review/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.ReviewList.as_view(), name='home'),
+    path('<slug:slug>/', views.review_detail, name='review_detail'),
 ]
 
--- review/views.py
+++ review/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Review
 
@@ -9,4 +9,30 @@ class ReviewList(generic.ListView):
     queryset = Review.objects.all()
     template_name = "review/index.html"
     paginate_by = 9
+
+
+def review_detail(request, slug):
+    """
+    Display the full review for the user
+
+    **Context**
+
+    ``review``
+        an instance of the :model: `review.Review`
+
+    **Template:**
+
+        :template:`review/review_detail.html`
+    """
+
+    review = get_object_or_404(Review, slug=slug)
+
+    return render(
+        request,
+        "review/review_detail.html",
+        {"review": review},
+    )
+
+    
+    
 
