bbe04d89dfbd7cc4e86175054dc030ead74ac2d5
Jamie McStay <jamiemcstay@hotmail.com>
2024-12-10 12:30:01 +0000

    Add AllAuth app

--- tunecritic/urls.py
+++ tunecritic/urls.py
@@ -19,8 +19,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include("review.urls"), name="review-urls"),
-    path('summernote/', include('django_summernote.urls')),
+    path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
 ]
 
 
