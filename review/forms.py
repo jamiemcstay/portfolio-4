15aa299b8f48f8b16f629f82ed56fd31856f7ca9
Jamie McStay <jamiemcstay@hotmail.com>
2024-12-18 13:20:00 +0000

    Add validation to form to only allow scores entered between 1 and 5

--- review/forms.py
+++ review/forms.py
@@ -1,7 +1,20 @@
 from django import forms
+from django.core.validators import MinValueValidator, MaxValueValidator
 from .models import Review
 
 class ReviewForm(forms.ModelForm):
+
+    score = forms.IntegerField(
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(5)
+        ],
+        widget=forms.NumberInput(attrs={
+            'min': 1,
+            'max': 5,
+            'placeholder': 'Enter a score between 1 and 5'
+        })
+    )
     class Meta:
         model = Review
         fields = [
